93bbe8a9bed9026988b812fc0092e92485285220
Enoh Collins <eno.col@gmail.com>
2023-11-27 18:56:01 +0000

    Fix indentation

--- parent_item.rb
+++ parent_item.rb
@@ -1,7 +1,7 @@
 class ParentItem
-attr_accessor :source, :archived
+  attr_accessor :source, :archived
 
-  def initialize(source, archived = false)
+  def initialize(source, archived: false)
     @source = source
     @archived = archived
   end
