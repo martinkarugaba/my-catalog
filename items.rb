0e103df03e0ede225d1d2aa0275de363c00d1e21
Enoh Collins <eno.col@gmail.com>
2023-11-27 18:38:27 +0000

    Fix indentation

--- items.rb
+++ items.rb
@@ -1,20 +1,20 @@
 require_relative "parent_item"
 
 class Item < ParentItem
-    def initialize(genre, author, source, label, date)
-        super(source, false)
-        @id = rand(1..1000)
-        @genre = genre
-        @author = author
-        @label = label
-        @publish_date = date
-    end
+  def initialize(genre, author, source, label, date)
+    super(source, false)
+    @id = rand(1..1000)
+    @genre = genre
+    @author = author
+    @label = label
+    @publish_date = date
+  end
 
-    def can_be_archived?()
-    end
+  def can_be_archived?()
+  end
 
-    def move_to_archive()
-    end
+  def move_to_archive()
+  end
 end
 
 item = Item.new('music', 'collins', 'source', 'readng', "2018-01-03")
