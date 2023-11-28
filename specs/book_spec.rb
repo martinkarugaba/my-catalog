e29715fc0842cc4d79a170b69fdbd3ba072bbb30
Martin Karugaba <martinkarugaba21@gmail.com>
2023-11-28 14:14:01 +0300

    Set up proper linters

---
+++ specs/book_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../book'
+
+describe Book do
+  describe '#initialize' do
+    it 'creates a new book object' do
+      book = Book.new('Title', 'Author', 2022)
+      expect(book).to be_instance_of(Book)
+    end
+  end
+end
