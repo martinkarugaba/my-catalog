816dee1e66780ede795135a1064ba603d757b63a
Enoh Collins <eno.col@gmail.com>
2023-11-28 11:02:09 +0000

    Fix linter

--- specs/book_spec.rb
+++ specs/book_spec.rb
@@ -1,5 +1,5 @@
 # linter reported an empty file so  i have just added to code to avoid the error
-#replace your code with new content and remove the comment
+# replace your code with new content and remove the comment
 
 class BookSpec
   def initialize
