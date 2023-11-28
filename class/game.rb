ae5be7fa681ad7d6d20a9244da976b3170fe2b2a
Enoh Collins <eno.col@gmail.com>
2023-11-28 15:58:34 +0000

    Add the Game class

---
+++ class/game.rb
@@ -0,0 +1,12 @@
+require_relative 'item'
+
+class Game < Item
+    def initialize(multiplayer, last_played_at)
+        @multiplayer = multiplayer
+        @last_played_at = last_played_at
+    end
+end
+
+game = Game.new(true, "2023-02-12") # I have created this game instance of the game class to test only
+
+# p game.can_be_archived? To test if this method (can_be_archived) had been properly inherited, remove the comment and run the file
