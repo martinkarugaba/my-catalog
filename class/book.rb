132de52a5bcfc6013a61be71c9f59027452ac36f
Martin Karugaba <martinkarugaba21@gmail.com>
2023-11-29 17:16:59 +0300

    Fix can be archived method

--- class/book.rb
+++ class/book.rb
@@ -1,14 +1,22 @@
-class Book
+require 'date'
+require_relative 'item'
+
+class Book < Item
   attr_accessor :publisher, :cover_state
 
   def initialize(publisher, cover_state)
+    super(publish_date='2021-01-01', archived: false)
     @publisher = publisher
     @cover_state = cover_state
   end
 
-  private
+  # private
 
   def can_be_archived?
     super || @cover_state == 'bad'
   end
 end
+
+martin = Book.new('Martin', 'bad')
+
+puts martin.can_be_archived?
