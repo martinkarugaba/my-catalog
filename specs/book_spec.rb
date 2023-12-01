419e8e1dc88494630097ad8427010ef0c5830303
Martin Karugaba <martinkarugaba21@gmail.com>
2023-12-01 15:32:52 +0300

    Fix tests

--- specs/book_spec.rb
+++ specs/book_spec.rb
@@ -1,175 +1,27 @@
 require 'rspec'
-require_relative 'book'
-require_relative 'app'
+require_relative '../classes/book'
 
 describe Book do
-  let(:book) { Book.new('Publisher', cover_state) }
-
-  describe '#can_be_archived?' do
-    context 'when cover_state is bad' do
-      let(:cover_state) { 'bad' }
-
-      it 'returns true' do
-        expect(book.send(:can_be_archived?)).to eq(true)
-      end
-    end
-
-    context 'when cover_state is not bad' do
-      let(:cover_state) { 'good' }
-
-      it 'returns false' do
-        expect(book.send(:can_be_archived?)).to eq(false)
-      end
-    end
+  before :each do
+    @book = Book.new('2000-01-01', 'Publisher', 'good')
   end
-end
 
-# test list all books
-describe App do
-  let(:app) { App.new }
-
-  describe '#list_all_books' do
-    before do
-      @original_stdout = $stdout
-      $stdout = StringIO.new
-    end
-
-    after do
-      $stdout = @original_stdout
-    end
-
-    it 'prints a message if there are no books' do
-      app.list_all_books
-      expect($stdout.string).to include('No books added')
-    end
-
-    it 'prints all books if there are any' do
-      book = Book.new('Publisher', 'Cover State', '2022-01-01')
-      app.add_book(book)
-      app.list_all_books
-      expect($stdout.string).to include('1-[Book], ID: ')
-      expect($stdout.string).to include(
-        ', Publisher: Publisher, Publish Date: 2022-01-01, Cover State: Cover State, Archived: '
-      )
-    end
+  it 'initializes with correct attributes' do
+    expect(@book.publisher).to eq('Publisher')
+    expect(@book.cover_state).to eq('good')
   end
-end
 
-# test add book to app
-describe App do
-  let(:app) { App.new }
-
-  describe '#add_book' do
-    before do
-      @original_stdout = $stdout
-      $stdout = StringIO.new
-    end
-
-    after do
-      $stdout = @original_stdout
-    end
-
-    it 'adds a book to the books array' do
-      expect { app.add_book('Publisher', 'Cover State', '2022-01-01') }.to change { app.books.length }.by(1)
-    end
-
-    it 'creates a book with the correct publisher, cover state, and publish date' do
-      app.add_book('Publisher', 'Cover State', '2022-01-01')
-      book = app.books.last
-      expect(book.publisher).to eq('Publisher')
-      expect(book.cover_state).to eq('Cover State')
-      expect(book.publish_date).to eq(Date.parse('2022-01-01'))
-    end
-
-    it 'prints a success message' do
-      app.add_book('Publisher', 'Cover State', '2022-01-01')
-      expect($stdout.string).to include('The book is added successfully ✅📖')
-    end
+  it 'can be archived if older than 10 years' do
+    expect(@book.can_be_archived?).to eq(true)
   end
-end
 
-# test ask load_books
-describe App do
-  let(:app) { App.new }
-
-  describe '#load_books' do
-    before do
-      @original_stdout = $stdout
-      $stdout = StringIO.new
-    end
-
-    after do
-      $stdout = @original_stdout
-    end
-
-    context 'when the file does not exist' do
-      before do
-        allow(File).to receive(:exist?).and_return(false)
-      end
-
-      it 'sets @books to an empty array' do
-        app.load_books
-        expect(app.books).to eq([])
-      end
-    end
-
-    context 'when the file exists' do
-      before do
-        allow(File).to receive(:exist?).and_return(true)
-      end
-
-      context 'when the file content is valid JSON' do
-        before do
-          allow(JSON).to receive(:parse).and_return([{ 'publish_date' => '2022-01-01', 'publisher' => 'Publisher',
-                                                       'cover_state' => 'Cover State' }])
-        end
-
-        it 'loads books from the file' do
-          app.load_books
-          book = app.books.first
-          expect(book.publish_date).to eq(Date.parse('2022-01-01'))
-          expect(book.publisher).to eq('Publisher')
-          expect(book.cover_state).to eq('Cover State')
-        end
-      end
-
-      context 'when the file content is not valid JSON' do
-        before do
-          allow(JSON).to receive(:parse).and_raise(JSON::ParserError)
-        end
-
-        it 'prints an error message and sets @books to an empty array' do
-          app.load_books
-          expect($stdout.string).to include('Failed to parse books:')
-          expect(app.books).to eq([])
-        end
-      end
-    end
+  it 'can be archived if cover state is bad' do
+    @book.cover_state = 'bad'
+    expect(@book.can_be_archived?).to eq(true)
   end
-end
 
-# test save books
-
-describe App do
-  let(:app) { App.new }
-
-  describe '#save_books' do
-    let(:file) { double('file') }
-
-    before do
-      allow(File).to receive(:open).and_yield(file)
-    end
-
-    it 'writes the books to the file in JSON format' do
-      book = Book.new('2022-01-01', 'Publisher', 'Cover State')
-      app.add_book(book)
-      expect(file).to receive(:write).with(JSON.generate([{
-                                                           'id' => book.id,
-                                                           'publish_date' => '2022-01-01',
-                                                           'publisher' => 'Publisher',
-                                                           'cover_state' => 'Cover State'
-                                                         }]))
-      app.save_books
-    end
+  it 'cannot be archived if less than 10 years old and cover state is good' do
+    @book.publish_date = Date.today.prev_year(9) # Set the publish date to 9 years ago
+    expect(@book.can_be_archived?).to eq(false)
   end
 end
--- specs/label_spec.rb
+++ specs/label_spec.rb
@@ -1,140 +1,40 @@
 require 'rspec'
-require_relative 'label'
-require_relative 'item'
+require_relative '../classes/label'
 
 describe Label do
-  let(:label) { Label.new(1, 'Title', 'Color') }
-  let(:item) { Item.new('2022-01-01') }
+  before :each do
+    @label = Label.new('Gift', 'red')
+  end
+
+  it 'has a title' do
+    expect(@label.title).to eq('Gift')
+  end
+
+  it 'has a color' do
+    expect(@label.color).to eq('red')
+  end
+
+  it 'has an id' do
+    expect(@label.id).to be_a(Integer)
+  end
+
+  it 'starts with no items' do
+    expect(@label.items).to eq([])
+  end
 
   describe '#add_item' do
-    it 'adds an item to the label' do
-      label.add_item(item)
-      expect(label.send(:items)).to include(item)
+    before :each do
+      @item = double('Item')
+      allow(@item).to receive(:label=)
+      @label.add_item(@item)
     end
 
-    it 'adds the label to the item' do
-      label.add_item(item)
-      expect(item.labels).to include(label)
-    end
-  end
-end
-
-# test add label to app
-
-describe App do
-  let(:app) { App.new }
-
-  describe '#add_label' do
-    it 'adds a label to the labels array' do
-      expect { app.add_label('Title', 'Color') }.to change { app.labels.length }.by(1)
+    it 'adds the item to the label' do
+      expect(@label.items).to include(@item)
     end
 
-    it 'creates a label with the correct title and color' do
-      app.add_label('Title', 'Color')
-      label = app.labels.last
-      expect(label.title).to eq('Title')
-      expect(label.color).to eq('Color')
-    end
-  end
-end
-
-# test list all labels
-
-describe App do
-  let(:app) { App.new }
-
-  describe '#list_all_labels' do
-    before do
-      @original_stdout = $stdout
-      $stdout = StringIO.new
-    end
-
-    after do
-      $stdout = @original_stdout
-    end
-
-    it 'prints a message if there are no labels' do
-      app.list_all_labels
-      expect($stdout.string).to include('No labels added')
-    end
-
-    it 'prints all labels if there are any' do
-      app.add_label('Title', 'Color')
-      app.list_all_labels
-      expect($stdout.string).to include('List of all labels:')
-      expect($stdout.string).to include('1. Label: Title, Color: Color')
-    end
-  end
-end
-
-# test load labels
-
-describe App do
-  let(:app) { App.new }
-
-  describe '#load_labels' do
-    context 'when the file does not exist' do
-      before do
-        allow(File).to receive(:exist?).and_return(false)
-      end
-
-      it 'sets @labels to an empty array' do
-        app.load_labels
-        expect(app.labels).to eq([])
-      end
-    end
-
-    context 'when the file exists' do
-      before do
-        allow(File).to receive(:exist?).and_return(true)
-      end
-
-      context 'when the file content is valid JSON' do
-        before do
-          allow(JSON).to receive(:parse).and_return([{ 'title' => 'Title', 'color' => 'Color' }])
-        end
-
-        it 'loads labels from the file' do
-          app.load_labels
-          label = app.labels.first
-          expect(label.title).to eq('Title')
-          expect(label.color).to eq('Color')
-        end
-      end
-
-      context 'when the file content is not valid JSON' do
-        before do
-          allow(JSON).to receive(:parse).and_raise(JSON::ParserError)
-        end
-
-        it 'raises a JSON::ParserError' do
-          expect { app.load_labels }.to raise_error(JSON::ParserError)
-        end
-      end
-    end
-  end
-end
-
-# save_labels
-
-describe App do
-  let(:app) { App.new }
-
-  describe '#save_labels' do
-    let(:file) { double('file') }
-
-    before do
-      allow(File).to receive(:open).and_yield(file)
-    end
-
-    it 'writes the labels to the file in JSON format' do
-      label = Label.new('Title', 'Color')
-      app.add_label(label)
-      expect(file).to receive(:write).with(JSON.generate([{
-                                                           'title' => 'Title',
-                                                           'color' => 'Color'
-                                                         }]))
-      app.save_labels
+    it 'sets the label of the item to the current label' do
+      expect(@item).to have_received(:label=).with(@label)
     end
   end
 end
