2e4108defb65b86aa9fb3dba2c52b4a98fb2daf9
Martin Karugaba <martinkarugaba21@gmail.com>
2023-12-01 15:39:36 +0300

    fix book and game class

--- classes/game.rb
+++ classes/game.rb
@@ -5,10 +5,10 @@ class Game < Item
   attr_accessor :publish_date, :multiplayer, :last_played_at
 
   def initialize(publish_date, multiplayer, last_played_at)
-    super(publish_date)
-    @multiplayer = multiplayer
-    @last_played_at = Date.parse(last_played_at)
-  end
+  super(publish_date, false) # pass false as the second argument
+  @multiplayer = multiplayer
+  @last_played_at = Date.parse(last_played_at)
+end
 
   def can_be_archived?
     super && (Date.today - @last_played_at > 2)
