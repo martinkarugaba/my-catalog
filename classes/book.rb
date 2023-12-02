a6e76b4a5954f428ef7b86f21c3f183bf0dac50b
Martin Karugaba <martinkarugaba21@gmail.com>
2023-12-02 15:46:55 +0300

    Fix item.rb

--- classes/book.rb
+++ classes/book.rb
@@ -1,10 +1,8 @@
-require_relative 'item'
-
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publish_date, publisher, cover_state)
-    super(publish_date, publisher)
+  def initialize(publish_date, publisher, cover_state, archived: false)
+    super(publish_date, archived: archived)
     @publisher = publisher
     @cover_state = cover_state
   end
--- classes/game.rb
+++ classes/game.rb
@@ -1,11 +1,10 @@
 require_relative 'item'
-require 'date'
 
 class Game < Item
   attr_accessor :publish_date, :multiplayer, :last_played_at
 
-  def initialize(publish_date, multiplayer, last_played_at)
-    super(publish_date, false) # pass false as the second argument
+  def initialize(publish_date, multiplayer, last_played_at, archived: false)
+    super(publish_date, archived: archived)
     @multiplayer = multiplayer
     @last_played_at = Date.parse(last_played_at)
   end
